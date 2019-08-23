446ca6708dd25f0aa51150ecc976834397cbb369
Wiradani R <adhan432@gmail.com>
2019-08-23 15:39:24 +0700

    Create Form9.php

---
+++ silawas/app/Form9.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Form9 extends Model
+{
+    protected $table = 'form9';
+
+    protected $fillable = [
+        'kapasitasPemeliharaan',
+        'populasiTernak',
+        'kategoriUsaha',
+        'totalProduksiSusu',
+        'wilayahPeredaran',
+        'jumlahKaryawan',
+        'P1-1',
+        'P1-2',
+        'P1-3',
+        'P1-4',
+        'P2',
+        'P3',
+        'P4',
+        'P5-1',
+        'P5-2',
+        'P7',
+        'P8',
+        'P9',
+        'P10',
+        'P11',
+        'P12',
+        'P13',
+        'P14',
+        'P15',
+        'P16',
+        'P17',
+        'P18',
+        'P19',
+      ];
+}

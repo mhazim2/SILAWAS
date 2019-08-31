79004fa2c13eef96a8d26f028c8503635a3a0beb
Wiradani R <adhan432@gmail.com>
2019-08-31 10:58:41 +0700

    new db

---
+++ silawas/app/Form1.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Form1 extends Model
+{
+    protected $table = 'form1';
+
+    protected $fillable = [
+        'kapasitasPemeliharaan',
+        'populasiTernak',
+        'kategoriUsaha',
+        'totalProduksiSusu',
+        'wilayahPeredaran',
+        'jumlahKaryawan',
+        'P1-1',
+        'P1-2',
+        'P1-3',
+        'P1-4',
+        'P2',
+        'P3',
+        'P4',
+        'P5-1',
+        'P5-2',
+        'P7',
+        'P8',
+        'P9',
+        'P10',
+        'P11',
+        'P12',
+        'P13',
+        'P14',
+        'P15',
+        'P16',
+        'P17',
+        'P18',
+        'P19',
+        'P1-1_ket',
+        'P1-2_ket',
+        'P1-3_ket',
+        'P1-4_ket',
+        'P2_ket',
+        'P3_ket',
+        'P4_ket',
+        'P5-1_ket',
+        'P5-2_ket',
+        'P7_ket',
+        'P8_ket',
+        'P9_ket',
+        'P10_ket',
+        'P11_ket',
+        'P12_ket',
+        'P13_ket',
+        'P14_ket',
+        'P15_ket',
+        'P16_ket',
+        'P17_ket',
+        'P18_ket',
+        'P19_ket',
+      ];
+}
---
+++ silawas/database/migrations/2019_08_30_111024_create_form1_table.php
@@ -0,0 +1,80 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateForm1Table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('form1', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('kapasitasPemeliharaan')->nullable();
+            $table->string('populasiTernak')->nullable();
+            $table->string("kategoriUsaha")->nullable();
+            $table->string("totalProduksiSusu")->nullable();
+            $table->string("wilayahPeredaran")->nullable();
+            $table->integer("jumlahKaryawan")->nullable();
+            $table->string("P1-1")->nullable();
+            $table->string("P1-2")->nullable();
+            $table->string("P1-3")->nullable();
+            $table->string("P1-4")->nullable();
+            $table->string("P2")->nullable();
+            $table->string("P3")->nullable();
+            $table->string("P4")->nullable();
+            $table->string("P5-1")->nullable();
+            $table->string("P5-2")->nullable();
+            $table->string("P7")->nullable();
+            $table->string("P8")->nullable();
+            $table->string("P9")->nullable();
+            $table->string("P10")->nullable();
+            $table->string("P11")->nullable();
+            $table->string("P12")->nullable();
+            $table->string("P13")->nullable();
+            $table->string("P14")->nullable();
+            $table->string("P15")->nullable();
+            $table->string("P16")->nullable();
+            $table->string("P17")->nullable();
+            $table->string("P18")->nullable();
+            $table->string("P19")->nullable();
+            $table->boolean("P1-1_ket")->nullable();
+            $table->boolean("P1-2_ket")->nullable();
+            $table->boolean("P1-3_ket")->nullable();
+            $table->boolean("P1-4_ket")->nullable();
+            $table->boolean("P2_ket")->nullable();
+            $table->boolean("P3_ket")->nullable();
+            $table->boolean("P4_ket")->nullable();
+            $table->boolean("P5-1_ket")->nullable();
+            $table->boolean("P5-2_ket")->nullable();
+            $table->boolean("P7_ket")->nullable();
+            $table->boolean("P8_ket")->nullable();
+            $table->boolean("P9_ket")->nullable();
+            $table->boolean("P10_ket")->nullable();
+            $table->boolean("P11_ket")->nullable();
+            $table->boolean("P12_ket")->nullable();
+            $table->boolean("P13_ket")->nullable();
+            $table->boolean("P14_ket")->nullable();
+            $table->boolean("P15_ket")->nullable();
+            $table->boolean("P16_ket")->nullable();
+            $table->boolean("P17_ket")->nullable();
+            $table->boolean("P18_ket")->nullable();
+            $table->boolean("P19_ket")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('form1');
+    }
+}

2d69fce1a6ffce9c314335ee328671859cff5477
OneStyd <wawansetyadi33@gmail.com>
2019-09-12 03:02:51 +0700

    rbac

--- silawas/app/Http/Controllers/PengawasansController.php
+++ silawas/app/Http/Controllers/PengawasansController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use App\UnitUsaha;
 use App\SurveyUnitUsaha;
 
@@ -14,11 +15,22 @@ class PengawasansController extends Controller
 {
     public function index()
     {
-        $listForms = DB::table('surveyunitusaha')
-            ->join('unitusaha', 'surveyunitusaha.idUnitUsaha', '=', 'unitusaha.id')
-            ->where('surveyunitusaha.idForm1','=',null)
-            ->select('surveyunitusaha.*','unitusaha.NamaUnitUsaha','unitusaha.PelakuUsaha_idPemilikUsaha', 'unitusaha.TahunOperasional')
-            ->get();
+        if (Auth::user()->accessRoleId == 1) {
+            $listForms = DB::table('surveyunitusaha')
+                ->join('unitusaha', 'surveyunitusaha.idUnitUsaha', '=', 'unitusaha.id')
+                ->where('surveyunitusaha.idForm1','=',null)
+                ->select('surveyunitusaha.*', 'unitusaha.NamaUnitUsaha', 'unitusaha.namaPemilikUsaha', 'unitusaha.PelakuUsaha_idPemilikUsaha', 'unitusaha.TahunOperasional')
+                ->get();
+        } else if (Auth::user()->accessRoleId == 7) {
+            $petugas = PengawasKesmavet::where('idUser', Auth::user()->id)->get();
+            $listForms = DB::table('surveyunitusaha')
+                ->join('unitusaha', 'surveyunitusaha.idUnitUsaha', '=', 'unitusaha.id')
+                ->where('idPengawas', '=', $petugas->idPengawasKesmavet)
+                ->orWhere('idPengawas2', '=', $petugas->idPengawasKesmavet)
+                ->orWhere('idPengawas3', '=', $petugas->idPengawasKesmavet)
+                ->select('surveyunitusaha.*', 'unitusaha.NamaUnitUsaha', 'unitusaha.namaPemilikUsaha', 'unitusaha.PelakuUsaha_idPemilikUsaha', 'unitusaha.TahunOperasional')
+                ->get();
+        }
         foreach ($listForms as $key=>$form) {
             if ($form->idForm1) {
                 $listForms[$key]->tipeForm = 'Tempat Budidaya Hewan Perah dan Pemerahan Susu';
